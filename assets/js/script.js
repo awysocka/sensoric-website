7a9a7be75d7cab1d0cd885c619cdfa1f807c3527
awysocka <wysocka.anna88@gmail.com>
2020-11-10 16:48:29 +0100

    SEN-28 & SEN-39 Make logo visible on scroll & add smooth scroll to links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
+//hamburger menu
+
 const hamburgerIcon = document.getElementById('hamburger');
 const navigationMenu = document.querySelector('#navigation-menu');
-const navigattionLinks = document.querySelectorAll('.navigation__item');
+const navigationLinks = document.querySelectorAll('.navigation__link');
 const navigationList = document.querySelector('#nav-list');
 
-hamburgerIcon.onclick = () => {
-    hamburgerIcon.classList.toggle('hamburger--active');
-    navigationMenu.classList.toggle('navigation__menu--active');
-    navigationList.classList.toggle('navigation__list--active');
-};
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerIcon.classList.toggle('hamburger--active');
+  navigationMenu.classList.toggle('navigation__menu--active');
+  navigationList.classList.toggle('navigation__list--active');
+}) ;
 
 const closeMenu = () => {
     hamburgerIcon.classList.remove('hamburger--active');
@@ -15,8 +17,8 @@ const closeMenu = () => {
     navigationList.classList.remove('navigation__list--active');
 };
 
-for (let navigationLink of navigattionLinks) {
-    navigationLink.onclick = closeMenu;
+for (let navigationLink of navigationLinks) {
+    navigationLink.addEventListener('click', closeMenu);
 }
 
 //accordion
@@ -38,5 +40,73 @@ function addAndRemoveClass(e) {
   
   clickedItem.classList.toggle('faq__list-item--active');
 }
+
+// Add logo to nav on scroll
+const navLogo = document.querySelector("#nav-logo");
+const heroLogo = document.querySelector('#hero-img');
+const nav = document.querySelector('.navigation');
+
+function updateNavOnScroll() {
+  const navHeight = nav.offsetHeight;
+  const heroLogoPosition = heroLogo.getBoundingClientRect();
+ 
+  if (navHeight >= heroLogoPosition.bottom) {
+    navLogo.classList.add("navigation__logo--visible");
+  } else {
+    navLogo.classList.remove("navigation__logo--visible");
+  }
+}
+
+window.addEventListener('scroll', updateNavOnScroll);
+window.addEventListener('deviceorientation', updateNavOnScroll);
+
+//smooth scroll
+
+const smoothScroll = event => {
+  event.preventDefault();
+  let targetID = event.target.getAttribute('href') || event.currentTarget.getAttribute('href');
+  targetID = targetID.substring(1);
+  const targetPosition = document.querySelector(targetID).offsetTop - 30;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 500;
+  let start = null;
+
+  const step = timestamp => {
+    if (!start) start = timestamp;
+    const runtime = timestamp - start;
+    const progress = Math.min(runtime / duration, 1);
+    const ease = easeOutQuad(progress);
+    window.scrollTo(0, startPosition + (distance * ease));
+    if (runtime < duration) window.requestAnimationFrame(step);
+  };
+
+  window.requestAnimationFrame(step);
+};
+
+const easeOutQuad = progress => {
+  return -progress * (progress - 2);
+};
+
+if (window.location.pathname === "/") {
+
+  const navLogoLink = document.querySelector('#nav-logo-link')
+  const arrowLink = document.querySelector('#arrow');
+
+  arrowLink.addEventListener('click', smoothScroll);
+  navLogoLink.addEventListener('click', smoothScroll);
+
+  navigationLinks.forEach(navigationLink => {
+
+    if(navigationLink.getAttribute('href').startsWith('/#')) {
+      navigationLink.addEventListener('click', smoothScroll);
+    }
+
+  });
+
+}
+
+
+
 
 
