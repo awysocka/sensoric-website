e534823fad33b960fa11935862102fe6c365dd3d
awysocka <wysocka.anna88@gmail.com>
2020-11-14 22:24:23 +0100

    SEN-17 Handle contact form submit in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,12 +111,16 @@ if (window.location.pathname === "/") {
 const form = document.querySelector('#contact-form');
 const nameInput = form.querySelector("#name");
 const nameValidationMessage = form.querySelector('#name-validation-message');
-const mailInput = form.querySelector("#mail");
-const mailValidationMessage = form.querySelector('#mail-validation-message');
+const emailInput = form.querySelector("#mail");
+const emailValidationMessage = form.querySelector('#email-validation-message');
 const messageInput = form.querySelector("#message");
 const messageValidationMessage = form.querySelector('#message-validation-message');
 const agreementCheckbox = form.querySelector("#agreement");
 const agreementValidationMessage = form.querySelector('#agreement-validation-message');
+const phoneNumberInput = form.querySelector('#phone');
+const submitResponse = form.querySelector('#submit-response');
+const successfulSubmitMessage = document.querySelector('.successful-submit-message');
+const contactFormTitle = document.querySelector('.heading-secondary');
 
 function nameInputValidation() {
   if (nameInput.value == null || nameInput.value === '') {
@@ -131,17 +135,17 @@ function nameInputValidation() {
 }
 
 function mailInputValidation() {
-  if (mailInput.value == null || mailInput.value === '') {
-    mailValidationMessage.innerText = 'Pole nie może być puste.';
-    mailInput.classList.add('invalid-value');
+  if (emailInput.value == null || emailInput.value === '') {
+    emailValidationMessage.innerText = 'Pole nie może być puste.';
+    emailInput.classList.add('invalid-value');
     return false;
-  } else if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(mailInput.value)) {
-    mailValidationMessage.innerText = 'Niepoprawny adres email';
-    mailInput.classList.add('invalid-value');
+  } else if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(emailInput.value)) {
+    emailValidationMessage.innerText = 'Niepoprawny adres email';
+    emailInput.classList.add('invalid-value');
     return false;
   } else {
-    mailValidationMessage.innerText = '';
-    mailInput.classList.remove('invalid-value');
+    emailValidationMessage.innerText = '';
+    emailInput.classList.remove('invalid-value');
     return true;
   }
 }
@@ -171,7 +175,7 @@ function agreementInputValidation() {
 }
 
 nameInput.addEventListener('input', nameInputValidation );
-mailInput.addEventListener('input', mailInputValidation );
+emailInput.addEventListener('input', mailInputValidation );
 messageInput.addEventListener('input', messageInputValidation );
 agreementCheckbox.addEventListener('input', agreementInputValidation );
 
@@ -183,8 +187,36 @@ form.addEventListener('submit', (e) => {
       && messageInputValidation() 
       && agreementInputValidation()
   ) {
-
-    alert('Wysłano');
-
-  } 
+    const data = {
+      name: nameInput.value,
+      email: emailInput.value,
+      phoneNumber: phoneNumberInput.value,
+      message: messageInput.value,
+      agreement: agreementCheckbox.checked
+    }
+  
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  
+    fetch('mail.php', options)
+      .then(response => {
+        if (response.status == 200) {
+          form.classList.add('hidden');
+          successfulSubmitMessage.classList.remove('hidden');
+          contactFormTitle.textContent = 'Potwierdzenie wysłania formularza';
+        } else {
+          submitResponse.innerText = 'Nie udało się wysłać wiadomości. Spróbuj jeszcze raz.';
+          console.error('error response: ', response);
+        }
+      })
+      .catch(error => {
+        submitResponse.innerText = 'Nie udało się wysłać wiadomości. Spróbuj jeszcze raz.';
+        console.error('error: ', error);
+      });
+  }
 });
